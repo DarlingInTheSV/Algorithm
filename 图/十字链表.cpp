7b59393cb719472a7012420c3aa7a0b50813fc99
lsy <862591869@qq.com>
2020-04-04 12:15:25 +0800

    更详细的注释

--- 图/十字链表.cpp
+++ 图/十字链表.cpp
@@ -5,8 +5,8 @@
 typedef char vex_type;
 struct G_node
 {
-    int head, tail, info; //head--相同弧头发出的,tail--相同弧尾发出的,info权重
-    G_node* h_link, * t_link;
+    int head, tail, info; //head--相同弧头发出的,tail--到达相同弧尾的,info权重
+    G_node* h_link, * t_link;//由h_link连起来的链表弧头相同，t_link连起来的链表到达相同的弧尾
     G_node() : head(0), tail(0), info(0), h_link(nullptr), t_link(nullptr) {};
 };
 struct G_list
@@ -19,19 +19,19 @@ struct G_list
 class Graph
 {
 public:
-    int vex_num, arc_num, * visited;
+    int vex_num, arc_num, * visited;//visited数组用于判断节点是否被访问过
     std::queue<G_list> q;
     G_list* vexs;
     Graph();
     ~Graph();
-    int get_num(vex_type& v);
+    int get_num(vex_type& v);//通过名称返回下标
     vex_type get_name(int index);//通过数组下标返回节点的名称
     void insert(int head, int tail, int info);
     void create();
     void show();
     void dfs(int index = 0);
     void bfs(int index = 0);
-    void clean_visited() { for (int i = 0; i < vex_num; ++i) visited[i] = 0; };
+    void clean_visited() { for (int i = 0; i < vex_num; ++i) visited[i] = 0; };//清空visited数组
 };
 
 Graph::Graph()
@@ -117,17 +117,17 @@ void Graph::show()
     }
 
 }
-void Graph::dfs(int index)
+void Graph::dfs(int index)//因为十字链表有方向，所以只需要一直使用h_link，就能遍历和一个节点相连的所有节点
 {
     std::cout << get_name(index) << std::endl;
     visited[index] = 1;
     G_node* p = vexs[index].out;
     while (p)
     {
-        int index1 =p->tail;
+        int index1 =p->tail;//对和每个节点相连的第一个节点递归使用dfs
         if (!visited[index1])
             dfs(index1);
-        p = p->h_link;
+        p = p->h_link;//回溯到和该节点相邻的其他节点
     }
 }
 
@@ -139,13 +139,13 @@ void Graph::bfs(int index)
         q.push(vexs[index]);
         while (!q.empty())
         {
-            G_node* tmp = q.front().out;
+            G_node* tmp = q.front().out;//指向队首元素
             int num = get_num(q.front().data);//获取节点在数组中的下标
             std::cout << q.front().data << std::endl;
             while (tmp)
             {
                 int index1 = tmp->tail;
-                if (!visited[index1])
+                if (!visited[index1])//若没有访问过，把所有和该节点相邻的节点入队
                 {
                     q.push(vexs[index1]);
                     visited[index1] = 1;
@@ -164,11 +164,16 @@ void Graph::bfs(int index)
 
 int main()
 {
+	//这里使用了C++的cin/cout重定向，直接读取文件并输出到文件，如果你不想使用，将下面的部分和结尾部分注释即可
+
+
     std::ifstream fin("十字链表测试.txt");//重定向输入为文件
     std::ofstream fout("十字链表测试__out.txt");//重定向输出
     std::streambuf* cin_backup = std::cin.rdbuf(fin.rdbuf());//cin.rdbuf(&)带参数返回原来的buffer,并备份
     std::streambuf* cout_backup = std::cout.rdbuf(fout.rdbuf());
 
+
+	//主程序
     Graph g;
     g.create();
     std::cout << "--------------------------初始视图--------------------" << std::endl;
@@ -184,9 +189,13 @@ int main()
     g.clean_visited();
     g.dfs();
 
+	//主程序
+
+
+	//恢复重定向
     std::cin.rdbuf(cin_backup);//不需要读取时，使用备份恢复cin cout到标准输入输出中
     std::cout.rdbuf(cout_backup);
-
+	//关闭文件读写
     fin.close();
     fout.close();
 }
--- 图/多重邻接表.cpp
+++ 图/多重邻接表.cpp
@@ -5,14 +5,14 @@
 typedef char vex_type;
 struct G_node
 {
-    int head, tail, info; //head--相同弧头发出的,tail--相同弧尾发出的,info权重
+    int head, tail, info; //info权重
     G_node* h_link, * t_link;//h——link指向与左边顶点同顶点的其他边，t_link指向与右边边顶点同顶点的其他边
     G_node() : head(0), tail(0), info(0), h_link(nullptr), t_link(nullptr) {};
 };
 struct G_list
 {
     vex_type data;
-    G_node* out; //无向，一个指向边的指针
+    G_node* out; //无向图，一个指向边的指针
     G_list() : data(0), out(nullptr) {};
 };
 
@@ -24,7 +24,7 @@ public:
     G_list* vexs;
     Graph();
     ~Graph();
-    int get_num(vex_type& v);
+    int get_num(vex_type& v);//通过数组名称返回节点的下标
     vex_type get_name(int index);//通过数组下标返回节点的名称
     void insert(int head, int tail, int info);
     void create();
@@ -120,10 +120,10 @@ void Graph::dfs(int index)
     std::cout << get_name(index) << std::endl;
     visited[index] = 1;
     G_node* p = vexs[index].out;
-    while (p)
+    while (p)//递归访问每个节点的第一条边
     {
-        int index1 = (p->head == index) ? p->tail : p->head;
-        if (!visited[index1])
+        int index1 = (p->head == index) ? p->tail : p->head;//因为是无向图，不能直接使用tail和head,需要判断该节点对应边中的哪个节点，比如1号节点第一条边是 (0,1,h_link,t_link) （重复利用了0号节点的边）
+        if (!visited[index1])								//则1号节点的下一条边是t_link指向的边，而0号节点的第一条边是h_link指向的边，也就是说要先判断当前顶点对应边中的哪个点，然后才能确定使用哪个指针
             dfs(index1);
         p = (p->head == index) ? p->h_link :p->t_link;
     }
@@ -163,11 +163,14 @@ void Graph::bfs(int index)
 
 int main()
 {
+	//这里使用了C++的cin/cout重定向，直接读取文件并输出到文件，如果你不想使用，将下面的部分和结尾部分注释即可
      std::ifstream fin("多重邻接表测试.txt");//重定向输入为文件
      std::ofstream fout("多重邻接表测试__out.txt");//重定向输出
      std::streambuf *cin_backup = std::cin.rdbuf(fin.rdbuf());//cin.rdbuf(&)带参数返回原来的buffer,并备份
      std::streambuf *cout_backup = std::cout.rdbuf(fout.rdbuf());
 
+
+	 //主程序
     Graph g;
     g.create();
     std::cout<<"--------------------------初始视图--------------------"<<std::endl;
@@ -184,12 +187,14 @@ int main()
     g.dfs();
 
 
+	//主程序
 
 
 
+	//恢复重定向
      std::cin.rdbuf(cin_backup);//不需要读取时，使用备份恢复cin cout到标准输入输出中
      std::cout.rdbuf(cout_backup);
-
+	 //关闭文件读写
      fin.close();
      fout.close();
 }
